280a8ddf8f24b70976cba61e647044cb0ad54fae
Aman Kapur <akapur07@gmail.com>
2019-12-27 17:30:56 +0530

    8080

--- index.js
+++ index.js
@@ -106,8 +106,7 @@ app.get('/stream/:infoHash.mp4', function(req, res, next) {
 });
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 const port = process.env.port || 3000;
-app.listen(server_port, server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", port " + server_port )
+app.listen(server_port, function () {
+    console.log( "Listening on ", port )
   });
